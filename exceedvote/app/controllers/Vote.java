7ec0b3d68f78725a36abb92196c55124199e5ae5
nookskill <nookskill@hotmail.com>
2015-03-05 00:26:19 +0700

    add bootstrap

---
+++ exceedvote/app/controllers/Vote.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.*;
+
+import views.html.*;
+
+public class Vote extends Controller{
+	public static Result index(){
+		return ok(vote.render("Vote Page"));
+	}
+}
