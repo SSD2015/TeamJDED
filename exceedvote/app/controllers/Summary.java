c861e36c72655e4cc7ea2b8d14c017f7a640221f
Waranyu Rerkdee <waranyu.1993@gmail.com>
2015-03-11 18:03:33 +0700

    add graph on summary page

--- exceedvote/app/controllers/Summary.java
+++ exceedvote/app/controllers/Summary.java
@@ -3,13 +3,13 @@ package controllers;
 import models.VoteModel;
 import play.mvc.Controller;
 import play.mvc.Result;
-import views.html.vote;
+import views.html.summaryscore;
 
 public class Summary extends Controller {
 	
 	public int point =1;
 	
 	public static Result index(){
-		return ok(vote.render(VoteModel.find.all()));
+		return ok(summaryscore.render(VoteModel.find.all()));
 	}
 }
