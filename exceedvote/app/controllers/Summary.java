e6d18010fcbce4a01ce233994754875540146d94
Aliz <garapoozz@hotmail.com>
2015-03-11 17:38:10 +0700

    Add dummy summary class

--- exceedvote/app/controllers/Summary.java
+++ exceedvote/app/controllers/Summary.java
@@ -1,5 +1,15 @@
 package controllers;
 
-public class Summary {
+import models.VoteModel;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.vote;
+
+public class Summary extends Controller {
 	
+	public int point =1;
+	
+	public static Result index(){
+		return ok(vote.render(VoteModel.find.all()));
+	}
 }
