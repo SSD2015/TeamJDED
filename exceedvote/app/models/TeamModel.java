3421a037058385e17cca336c6bd74e3885973636
Aliz <garapoozz@hotmail.com>
2015-03-31 17:23:00 +0700

    Edit team model

--- exceedvote/app/models/TeamModel.java
+++ exceedvote/app/models/TeamModel.java
@@ -1,12 +1,22 @@
 package models;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import play.db.ebean.Model.Finder;
+
+@Entity
 public class TeamModel {
+	@Id
+	public int id;
+	
 	public String teamname;
 	
 	public String teamdescription;
 	
 	public String pic;
 
+	public static Finder<Long, TeamModel> find = new Finder<Long, TeamModel>(Long.class, TeamModel.class);
 	
 	public TeamModel(String teamname, String teamdescription , String pic) {
 		this.teamname = teamname;
