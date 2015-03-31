7530b0301abd5e6076db5a181d0e093fa944b837
Aliz <garapoozz@hotmail.com>
2015-03-31 18:18:58 +0700

    add Team Management

--- exceedvote/app/controllers/TeamManagement.java
+++ exceedvote/app/controllers/TeamManagement.java
@@ -1,12 +1,20 @@
 package controllers;
 
-import play.mvc.Result;
+import models.TeamModel;
 import views.html.*;
+import play.mvc.*;
+import play.data.Form;
 
-public class TeamManagement {
+public class TeamManagement extends Controller {
 	public static Result index(){
 		return ok(teammanagement.render());
 	}
 	
-	
+	public static Result AddInform(){
+		TeamModel team = Form.form(TeamModel.class).bindFromRequest().get();
+		
+		team.save();
+		
+		return index();
+	}
 }
--- exceedvote/app/models/TeamModel.java
+++ exceedvote/app/models/TeamModel.java
@@ -10,51 +10,15 @@ public class TeamModel {
 	@Id
 	public int id;
 	
-	public String teamname;
+	public int score;
+	
+	public String name;
 	
 	public String teamdescription;
 	
 	public String pic;
 
 	public static Finder<Long, TeamModel> find = new Finder<Long, TeamModel>(Long.class, TeamModel.class);
-	
-	public TeamModel(String teamname, String teamdescription , String pic) {
-		this.teamname = teamname;
-		this.teamdescription = teamdescription;
-		this.pic = pic;
-	}
-	
-	public String getTeamname() {
-		return teamname;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public void setTeamname(String teamname) {
-		this.teamname = teamname;
-	}
-
-	public String getTeamdescription() {
-		return teamdescription;
-	}
-
-	public void setTeamdescription(String teamdescription) {
-		this.teamdescription = teamdescription;
-	}
-
-	public String getPic() {
-		return pic;
-	}
-
-	public void setPic(String pic) {
-		this.pic = pic;
-	}
 	
 	
 }
