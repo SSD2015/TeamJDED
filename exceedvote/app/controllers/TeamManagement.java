3343c7a2a7f7e54316bdb07ea0dd414e2c66ab8a
Waranyu Rerkdee <waranyu.1993@gmail.com>
2015-03-31 18:04:58 +0700

    edit UI team management3

--- exceedvote/app/controllers/TeamManagement.java
+++ exceedvote/app/controllers/TeamManagement.java
@@ -1,9 +1,10 @@
 package controllers;
 
+import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.*;
 
-public class TeamManagement {
+public class TeamManagement extends Controller{
 	public static Result index(){
 		return ok(teammanagement.render());
 	}
