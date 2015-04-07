9f1e37b9c207b45b7dc563e525be3aa6dfe4c943
Aliz <garapoozz@hotmail.com>
2015-04-07 18:48:20 +0700

    edit usermodel

--- exceedvote/app/models/UserModel.java
+++ exceedvote/app/models/UserModel.java
@@ -21,51 +21,5 @@ public class UserModel {
 	
 	public static Finder<Long, UserModel> find = new Finder<Long, UserModel>(Long.class, UserModel.class);
 	
-	public UserModel(String username, String password) {
-		this.username = username;
-		this.password = password;
-		this.isAdmin = false;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public boolean isAdmin() {
-		return isAdmin;
-	}
-
-	public void setAdmin(boolean isAdmin) {
-		this.isAdmin = isAdmin;
-	}
-	
 	
 }
