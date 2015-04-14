a5bbf22a596ca81eb92b43b99221b8ef0ae1408e
nookskill <nookskill@hotmail.com>
2015-04-14 14:56:47 +0700

    update criteria system

--- exceedvote/app/controllers/TeamManagement.java
+++ exceedvote/app/controllers/TeamManagement.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import static play.data.Form.form;
+import models.CriteriaModel;
 import models.TeamModel;
 import play.mvc.Controller;
 import play.mvc.Result;
@@ -9,12 +10,12 @@ import play.mvc.*;
 import play.data.Form;
 import play.mvc.Security;
 
-
 public class TeamManagement extends Controller{
 	
 	@Security.Authenticated(Secured.class)
 	public static Result index(){
-		return ok(teammanagement.render(Secured.isLoggedIn(ctx()), Secured.getUserInfo(ctx()),Secured.isAdmin(ctx()),TeamModel.find.all()));
+		return ok(teammanagement.render(Secured.isLoggedIn(ctx()), Secured.getUserInfo(ctx()),Secured.isAdmin(ctx())
+				,TeamModel.find.all(),CriteriaModel.find.all()));
 	}
 	
 	@Security.Authenticated(Secured.class)
@@ -55,6 +56,33 @@ public class TeamManagement extends Controller{
 	
 	public static Result deleteTeam(Long id){
 		TeamModel.find.ref(id).delete();
+		
+		return index();
+	}
+	
+	
+	public static Result recordCriteria(){
+		CriteriaModel criteria = Form.form(CriteriaModel.class).bindFromRequest().get();
+		
+		criteria.save();
+		
+		return index();
+	}
+	
+	public static Result updateCriteria(Long id ){
+		Form<CriteriaModel> criForm = form(CriteriaModel.class).bindFromRequest();
+        if(criForm.hasErrors()) {
+            return index();
+        }
+        criForm.get().update(id);
+        
+        //flash("success", "Team " + teamForm.get().name + " has been updated");
+		
+		return index();
+	}
+	
+	public static Result deleteCriteria(Long id){
+		CriteriaModel.find.ref(id).delete();
 		
 		return index();
 	}
