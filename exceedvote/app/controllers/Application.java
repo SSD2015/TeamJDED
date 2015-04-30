1eea9e3a1261c9a755306c9de38b0e8456447330
Aliz <garapoozz@hotmail.com>
2015-04-30 11:21:05 +0700

    Create class authenticator but not work T^T

--- exceedvote/app/controllers/Application.java
+++ exceedvote/app/controllers/Application.java
@@ -12,6 +12,8 @@ import play.mvc.Security;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+import com.sun.jna.platform.win32.Netapi32Util.User;
+
 
 public class Application extends Controller {
 
@@ -55,8 +57,40 @@ public class Application extends Controller {
 		    }
 	    }
 	}
-    
-    
+	public static class Login {
+        
+        public String username;
+        public String password;
+        
+        public String validate() {
+            if(User.authenticate(username, password) == null) {
+                Logger.info(username + " " + password);
+                return "Invalid user or password";
+            }
+            return null;
+        }
+        
+    }
+	
+	public static Result authenticate( ) {
+	    Form<Login> loginForm = Form.form(Login.class);
+	    loginForm = loginForm.bindFromRequest();
+	    if (loginForm.hasErrors()) {
+	         return badRequest(views.html.index.render(loginForm));
+	    }
+
+	    Login data = loginForm.get();
+	    // authenticate the username using password
+	    // if this fails it returns null
+	    User user = User.authenticate( data.username, data.password);
+	    if (user == null) {
+	        flash("error", "Username or password incorrect.");
+	        return badRequest(views.html.index.render(loginForm));
+	    }
+	    // success! Create a session for this user.
+	    
+	}
+	
     @Security.Authenticated(Secured.class)
 	public static Result logout() {
 		session().clear();
--- exceedvote/app/controllers/Authenticator.java
+++ exceedvote/app/controllers/Authenticator.java
@@ -2,7 +2,7 @@ package controllers;
 
 import com.sun.jna.platform.win32.Netapi32Util.User;
 
-public class Authenticator {
+public abstract class Authenticator {
 	public static Authenticator getInstance() {
 		//TODO make this a singleton
 		return new DatabaseAuthenticator();
